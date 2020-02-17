ad9082584f9f3aa64ab1e7feb45f0856a7461040
Katleho MOFOKENG <kamofoke@c5r6s9.wethinkcode.co.za>
2020-02-17 08:38:45 +0200

    header

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: kamofoke <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/02/13 07:54:33 by kamofoke          #+#    #+#              #
-#    Updated: 2020/02/13 07:55:43 by kamofoke         ###   ########.fr        #
+#    Updated: 2020/02/17 08:38:17 by kamofoke         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,8 +14,24 @@ NAME	= 	minishell
 
 FLAGS	=	gcc -Wall -Wextra -Werror
 
-SRC 	=	
+SRC 	=	ft_functions.c ft_cd.c ft_unsetenv.c
 
 OBJ 	=	$(SRC:.c=.o)
 
 all	:	$(NAME)
+
+$(NAME)	:
+		@make -C libft/
+		@$(FLAGS) -c $(SRC)
+		@$(FLAGS) -o $(NAME) $(SRC) -L libft/ -lft
+
+clean	:
+		@make clean -C libft/
+		@rm -rf $(OBJ)
+
+fclean	: 	clean
+		@make fclean -C libft/
+		@rm -rf $(NAME)
+
+re	: 	fclean all
+
---
+++ minishell.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamofoke <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/17 08:25:35 by kamofoke          #+#    #+#             */
+/*   Updated: 2020/02/17 08:29:27 by kamofoke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include "./libft/libft.h"
+
+char 	**g_envp;
+int		exec_bin(char **arg);
+int		len_of_envp(char **envp);
+int		ft_echo(char **arg);
+int		ft_setenv(char **arg);
+int		ft_unsetenv(char **arg);
+char	*find_envp(char *str);
+int		find_position(char *str);
+int		ft_cd(char **arg);
+void	set_envp(char *arg, char *value);
+
+#endif
